85f950180e885e379b92f6214de5a2c3a4a0f2f3
Ahad <abdulahad2a8@gmail.com>
2025-02-23 17:00:45 +0500

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,14 @@ module.exports = {
         FRed: '#8E1616',
         FRedLighter: '#D84040',
         FText: '#D84040',
-        FText1: '#fbfd00',
+        FText1: '#c9f422',
+        ChangedThemeColor : '#c9f422',
       },
 
       fontFamily : {
         Poppins : ['Poppins','sans-serif'],
         LogoFont : ['UnifrakturMaguntia','sans-serif'],
-        LogoFont1:['Clicker Script','sans-serif'],
+        HeadingFonts:['Teko','sans-serif'],
       },
       boxShadow : {
         'customRed': 'inset 0px 0px 15px 10px rgba(255, 0, 0, 0.2)',
